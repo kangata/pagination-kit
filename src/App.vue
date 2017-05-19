692d3a79ff580c9ddb2fb84b7974cbb63039ba7c
Ata <ata@Quetzal-2.local>
2017-05-19 10:17:29 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="uk-container uk-margin-medium-top uk-margin-medium-bottom   ">
+        <h4 class="uk-heading-line uk-text-center"><span>UIkit 3 default</span></h4>
+        <pagination :pagination="pagination" v-on:change-page="changePage"></pagination>
+
+        <h4 class="uk-heading-line uk-text-center"><span>UIkit 3 no info</span></h4>
+        <pagination :pagination="pagination" v-on:change-page="changePage" :info="false"></pagination>
+
+        <h4 class="uk-heading-line uk-text-center"><span>UIkit 3 custom page show</span></h4>
+        <pagination :pagination="pagination" v-on:change-page="changePage" :page-show="pageShow"></pagination>
+
+        <h4 class="uk-heading-line uk-text-center"><span>UIkit 3 position left</span></h4>
+        <pagination :pagination="pagination" v-on:change-page="changePage" position="left"></pagination>
+
+        <h4 class="uk-heading-line uk-text-center"><span>UIkit 3 position right</span></h4>
+        <pagination :pagination="pagination" v-on:change-page="changePage" position="right"></pagination>
+    </div>
+</template>
+
+<script>
+import Pagination from './UIkit-3.vue'
+
+export default {
+    name: 'PaginationKit',
+
+    components: {
+        Pagination
+    },
+
+    data () {
+        return {
+            pagination: {
+                current_page: 1,
+                from: 1,
+                last_page: 25,
+                per_page: 4,
+                to: 1,
+                total: 100
+            },
+            pageShow: 5
+        }
+    },
+
+    methods: {
+        changePage(page) {
+            console.log(page)
+        }
+    }
+}
+</script>
---
+++ src/UIkit-3.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <ul v-if="paginationNumbers" class="uk-pagination uk-margin-small-top uk-margin-small-bottom" :class="flexPosition">
+            <li :class="{ 'uk-disabled': pagination.current_page == 1 }">
+                <a href="" @click.prevent="changePage(pagination.current_page - 1)">
+                    <span uk-pagination-previous></span>
+                </a>
+            </li>
+            <li v-if="from > 1">
+                <a href="" @click.prevent="changePage(1)">1</a>
+            </li>
+            <li class="uk-disable" v-if="from > 1">
+                <span>...</span>
+            </li>
+            <li v-for="page in paginationNumbers" :class="{ 'uk-active': page == pagination.current_page }">
+                <a href="" @click.prevent="changePage(page)">{{ page }}</a>
+            </li>
+            <li class="uk-disable" v-if="to < pagination.last_page">
+                <span>...</span>
+            </li>
+            <li v-if="to < pagination.last_page">
+                <a href="" @click.prevent="changePage(pagination.last_page)">{{ pagination.last_page }}</a>
+            </li>
+            <li :class="{ 'uk-disabled': pagination.last_page == pagination.current_page }">
+                <a href="" @click.prevent="changePage(pagination.current_page + 1)">
+                    <span uk-pagination-next></span>
+                </a>
+            </li>
+        </ul>
+        <small v-if="info" class="uk-flex" :class="flexPosition ">
+            Page {{ pagination.current_page }} of {{ pagination.last_page }}
+        </small>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PaginationKit',
+
+    props: {
+        pagination: {
+            type: Object,
+            required: true
+        },
+        pageShow: {
+            type: Number,
+            default: 7
+        },
+        info: {
+            type: Boolean,
+            default: true
+        },
+        position: {
+            type: String,
+            default: 'center'
+        }
+    },
+
+    data () {
+        return {
+            from: 1,
+            to: 1
+        }
+    },
+
+    computed: {
+        paginationNumbers () {
+            if (!this.pagination.to) {
+                return []
+            }
+
+            if (this.pageShow % 2 == 0) console.warn('The number of `pageShow` is even, try the odd number.')
+
+            if (this.pageShow > this.pagination.last_page) console.error('Your `pagination.last_page` less than `pageShow`')
+
+            if (this.pagination.current_page > Math.ceil(this.pageShow / 2)) {
+                this.from = this.pagination.current_page - Math.floor(this.pageShow / 2);
+            }
+            
+            if (this.pagination.last_page - this.pagination.current_page < Math.floor(this.pageShow / 2)) {
+                this.from = this.pagination.last_page - (this.pageShow - 1);
+            }
+
+            this.to = this.from + (this.pageShow - 1);
+            
+            if (this.to >= this.pagination.last_page) {
+                this.to = this.pagination.last_page
+            }
+
+            let numbers = []
+
+            for (let page = this.from; page <= this.to; page++) {
+                numbers.push(page)
+            }
+
+            return numbers
+        },
+
+        flexPosition () {
+            let position = 'uk-flex-center'
+
+            if (this.position == 'left') position = 'uk-flex-left'
+            if (this.position == 'right') position = 'uk-flex-right'
+
+            return position
+        }
+    },
+
+    methods: {
+        changePage (page) {
+            document.activeElement.blur()
+
+            this.pagination.current_page = page
+            
+            this.$emit('change-page', page)
+        }
+    }
+}
+</script>
